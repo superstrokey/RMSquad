e295b22a56a328fea2fc4f72c60ae32298ba6a39
superstrokey <etienne.scraire@gmail.com>
2019-04-02 17:22:22 -0400

    settings and proper launcher

--- rmsquad.rb
+++ rmsquad.rb
@@ -19,17 +19,20 @@ Built with Shoes (#{Shoes::RELEASE_NAME}, v#{Shoes::RELEASE_ID})
 
 Icons by Material design
 Available under Apache license version 2.0.},
-
+    :cancel => %{Cancel},
     :close => %{Close Application},
-    :encrypted =>
+    :launcher_encrypted =>
 %{This project is encrypted, RMSquad would be pretty useless here.},
-    :initialize? => 
+    :launcher_initialize? => 
 %{It looks like this project is not RMSquad initialized yet. Proceed?
 (This is a non destructive operation, it simply adds files and deletes none)},
-    :info => %{What is this?},
-    :invalid_folder =>
+    :launcher_info => %{What is this?},
+    :launcher_invalid_folder =>
 %{It appears that the given folder was not a valid RPG Maker Vx Ace project folder.},
-    :open_folder => %{Open Folder}
+    :launcher_open_folder => %{Open Folder},
+    :settings_lfs => %{Use git LFS to manage assets?},
+    :settings_data => %{Commit data files to repository?},
+    :validate => %{Validate}
 }
 
 ALL_STRINGS = {
@@ -40,22 +43,48 @@ ALL_STRINGS = {
 STRINGS = ALL_STRINGS[SETTINGS[:language]]
 
 #=======================================================================================================#
-module Utils
-    def self.encrypted?(folder)
-        Dir.entries(folder).include? "Game.rgss3a"
+class SettingsWrapper
+    attr_accessor :commit_data
+    attr_accessor :use_lfs
+
+    def initialize
+        @commit_data = false
+        @use_lfs = false
     end
 
-    def self.initialized?(folder)
-        root_content = Dir.entries(folder)
+    def dump
+        YAML.dump({"commit_data" => @commit_data, "use_lfs" => @use_lfs})
+    end
+
+    def load(data)
+        settings = YAML.load(data)
+        @commit_data = settings["commit_data"]
+        @use_lfs = settings["use_lfs"]
+    end
+end
+#=======================================================================================================#
+class VxProject
+    attr_reader :folder
+
+    def initialize(folder)
+        @folder = folder
+    end
+
+    def encrypted?
+        Dir.entries(@folder).include? "Game.rgss3a"
+    end
+
+    def initialized?
+        root_content = Dir.entries(@folder)
         ["RMSquad", "RMSquad.yml"].collect { |f| root_content.include?(f) }.all?
     end
 
-    def self.valid_vx_ace?(folder)
+    def valid_vx_ace?
         valid = true
-        root_content = Dir.entries folder
+        root_content = Dir.entries @folder
         ["Audio", "Data", "Graphics", "System", "Game.rvproj2"].each { |f| valid &= root_content.include?(f) }
         if valid
-            data_folder = File.join(folder, "Data")
+            data_folder = File.join(@folder, "Data")
             data_files = ["Actors", "Animations", "Armors", "Classes", "CommonEvents", "Enemies",
                           "Items", "MapInfos", "Scripts", "Skills", "States", "System", "Tilesets",
                           "Troops", "Weapons"]
@@ -66,8 +95,7 @@ module Utils
     end
 end
 #=======================================================================================================#
-
-class Locker
+class Lock
     def initialize
         @locked = true
     end
@@ -85,45 +113,80 @@ class Locker
         end
     end
 end
+
+#=======================================================================================================#
+
+def setup(project)
+    if project.initialized?
+        launch project.folder
+        return
+    end
+
+    if confirm STRINGS[:launcher_initialize?]
+        Dir.mkdir(File.join(project.folder, "RMSquad"))
+        Shoes.app(title: "RMSquad Project Configuration",  height: 320, width: 640) do 
+            stack(margin: 10) do
+                settings = SettingsWrapper.new
+                flow(margin: 10) do
+                    @check_use_lfs = check
+                    para STRINGS[:settings_lfs]
+                end
+                flow(margin: 10) do
+                    @check_commit_data = check
+                    para STRINGS[:settings_data]
+                end
+                flow(margin: 10) do
+                    button STRINGS[:cancel] { close() }
+                    button STRINGS[:validate] do
+                        settings.commit_data = @check_commit_data.checked?
+                        settings.use_lfs = @check_use_lfs.checked?
+                        File.open(File.join(project.folder, "RMSquad.yml"), "w") do |f|
+                            f.write(settings.dump)
+                        end
+                        File.open(File.join(project.folder, ".gitattributes"), "a+") do |f|
+                        end
+                        File.open(File.join(project.folder, ".gitignore"), "a+") do |f|
+                        end
+                        launch project.folder
+                        close()
+                    end
+                end
+            end
+        end
+    end
+end
+
+#=======================================================================================================#
 # Validate ensures the folder is ok for use
 def validate(folder)
+    project = VxProject.new folder
     # Make sure it's not an encrypted project
-    if Utils.encrypted? folder
-        alert STRINGS[:encrypted]
-        return false
+    if project.encrypted?
+        alert STRINGS[:launcher_encrypted]
+        return
     end
 
     # Make sure it's a valid vx ace folder
-    unless Utils.valid_vx_ace? folder
-        alert STRINGS[:invalid_folder]
-        return false
+    unless project.valid_vx_ace?
+        alert STRINGS[:launcher_invalid_folder]
+        return
     end
-
-    # Initialize project if it wasn't done
-    unless Utils.initialized? folder
-        if confirm STRINGS[:initialize?]
-            Dir.mkdir(File.join(folder, "RMSquad"))
-            File.open(File.join(folder, "RMSquad.yml"), "w") do |f|
-            end
-        else
-            return false
-        end
-    end
-    true
+    # Setup project for use
+    setup project
 end
 
+#=======================================================================================================#
 # Shoes definition for RMSquad Main Application
 def launch(folder)
-    return unless validate folder
     # Main Application definition
     Shoes.app(title: "RMSquad | #{File.basename(folder)}", height: 768, width: 1024, resizable: true) do
-        @locker = Locker.new
+        @lock = Lock.new
         stack(margin: 10) do
             # Lock transaction
             flow(margin: 10) do
                 background gainsboro
                 @lock_icon = image File.join("images", "refresh.png")
-                @lock_icon.path = @locker.get_icon
+                @lock_icon.path = @lock.get_icon
                 @lock_checkbox = check { toggle_lock }
                 @lock_checkbox.style margin: 5
                 @lock_checkbox.checked = true
@@ -131,11 +194,12 @@ def launch(folder)
         end
 
         def toggle_lock
-            @locker.toggle!
-            @lock_icon.path = @locker.get_icon
+            @lock.toggle!
+            @lock_icon.path = @lock.get_icon
         end
     end
 end
+
 #=======================================================================================================#
 # Shoes definition for RMSquad Launcher
 Shoes.app(title: "RMSquad Launcher",  height: 320, width: 640) do
@@ -144,13 +208,13 @@ Shoes.app(title: "RMSquad Launcher",  height: 320, width: 640) do
         # Open a folder
         flow(margin: 10) do
             image File.join("images", "open.png"), margin: 10
-            b = button STRINGS[:open_folder] { launch ask_open_folder }
+            b = button STRINGS[:launcher_open_folder] { validate ask_open_folder }
             b.style height: 1.0, width: 0.75
         end
         # Release Info
         flow(margin: 10) do
             image File.join("images", "info.png"), margin: 10
-            b = button STRINGS[:info] { alert STRINGS[:application_info] }
+            b = button STRINGS[:launcher_info] { alert STRINGS[:launcher_application_info] }
             b.style height: 1.0, width: 0.75
         end
         # Close (until I get a better button to put there)
